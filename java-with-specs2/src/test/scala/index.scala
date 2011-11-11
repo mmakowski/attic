b91e9f31a3ab6ac39cc787c540e3117b9510c946
Maciek Makowski <maciek.makowski@gmail.com>
2011-11-11 23:23:26 +0000

    removed java-with-specs2 (now part of maven-specs2-plugin)

--- java-with-specs2/src/test/scala/index.scala
+++
@@ -1,8 +0,0 @@
-import org.specs2._
-import runner.FilesRunner._
-
-class index extends Specification { def is =
-  examplesLinks("Test specifications")
-
-  def examplesLinks(t: String) = specifications().foldLeft(t.title) { (res, cur) => res ^ see(cur) }
-}
